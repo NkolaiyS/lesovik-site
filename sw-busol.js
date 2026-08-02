f9dde648149f28623d3b06e13b5fcde596b72602
Lady's <foltaim@mail.ru>
2026-08-02 10:24:59 +0300

    Update sw-busol.js

--- sw-busol.js
+++ sw-busol.js
@@ -1,4 +1,4 @@
-const BUSOL_CACHE = 'busol-hns-v13.2'; // Повысим версию кэша, чтобы заставить браузер обновиться!
+const BUSOL_CACHE = 'busol-hns-v13.3'; // Повысим версию кэша, чтобы заставить браузер обновиться!
 const BUSOL_ASSETS = [
   '/busol-mobile.html',
   '/logo.jpeg',
