026478fc3dc2ac477697d2fe9f4f04cd8cab8fc5
Lady's <foltaim@mail.ru>
2026-09-09 23:23:00 +0300

    Update sw-tracker.js

--- sw-tracker.js
+++ sw-tracker.js
@@ -1,5 +1,5 @@
 // Service Worker для автономного Навигатора Охотника и Рыбака
-const CACHE_NAME = 'lesovik-taktik-v7-7';
+const CACHE_NAME = 'lesovik-taktik-v7-8';
 const ASSETS_TO_CACHE = [
   '/tracker.html',
   '/tracker.webmanifest',
